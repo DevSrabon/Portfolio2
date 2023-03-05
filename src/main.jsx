dabb289bb19ba9c767759eef8e9328293eb186c3
DevSrabon <et.srabon@gmail.com>
2023-03-05 23:54:31 +0600

    added scroll

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "react-scroll-to-top";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
+		<ScrollToTop style={{width:"30px"}} smooth color="#6f00ff" />
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
